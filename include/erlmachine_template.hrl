68bf93d696e2ae08fe5e57e0423253bb74e670c0
erlmachinedev <erlmachinedev@gmail.com>
2022-01-10 21:58:24 +0200

    Exported iterator type

--- include/erlmachine_template.hrl
+++ include/erlmachine_template.hrl
@@ -1,4 +1,6 @@
 -type template() :: erlmachine_template:template().
+-type iterator() :: erlmachine_template:iterator().
+
 
 -type path() :: erlmachine_template:path().
 -type spec() :: erlmachine_template:spec().
